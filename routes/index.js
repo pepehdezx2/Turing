ae926cf7dc1f4f003c84cbd59b16764fe61c3691
José Antonio Hernández <pepehdezx2@gmail.com>
2020-08-02 15:52:58 -0500

    Views

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,32 @@ router.get('/dashboard', ensureAuthenticated, (req, res) =>
   })
 );
 
+// account view
+router.get('/account', ensureAuthenticated, (req, res) =>
+  res.render('account.ejs', {
+    name: req.user.name
+  })
+);
+
+// lockers view
+router.get('/lockers', ensureAuthenticated, (req, res) =>
+  res.render('lockers.ejs', {
+    name: req.user.name
+  })
+);
+
+// donations view
+router.get('/donations', ensureAuthenticated, (req, res) =>
+  res.render('donations.ejs', {
+    name: req.user.name
+  })
+);
+
+// donations view
+router.get('/reservations', ensureAuthenticated, (req, res) =>
+  res.render('reservations.ejs', {
+    name: req.user.name
+  })
+);
+
 module.exports = router;
