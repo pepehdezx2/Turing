87661cce3b4b03368d770f5519cfc283de8700e1
José Antonio Hernández <pepehdezx2@gmail.com>
2020-08-02 23:20:01 -0500

    Admin features

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require("path");
-const Locker = require("../models/Locker");
+const Lockerdb = require("../models/Locker");
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
 //Welcome page
@@ -10,14 +10,16 @@ router.get('/', forwardAuthenticated, (req, res) => res.render('welcome.ejs'));
 //Account page
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
   res.render('dashboard.ejs', {
-    name: req.user.name
+    name: req.user.name,
+    isAdmin: req.user.isAdmin
   })
 );
 
 // account view
 router.get('/account', ensureAuthenticated, (req, res) =>
   res.render('account.ejs', {
-    name: req.user.name
+    name: req.user.name,
+    isAdmin: req.user.isAdmin
   })
 );
 
@@ -25,114 +27,69 @@ router.get('/account', ensureAuthenticated, (req, res) =>
 router.get('/lockers', ensureAuthenticated, (req, res) =>
   res.render('lockers.ejs', {
     name: req.user.name,
+    isAdmin: req.user.isAdmin,
+    locker: req.user.locker
+  })
+);
+
+// lockers post
+router.post('/lockers', ensureAuthenticated, (req, res) =>
+  res.render('lockers.ejs', {
+    name: req.user.name,
+    isAdmin: req.user.isAdmin,
     locker: req.user.locker
   })
 );
 
 // locker rental
 
-/*router.post('/lockers', (req, res) =>{
-  const isAvailable = true;
+//router.post('/lockers', ensureAuthenticated, (req, res) =>{
+ /* const isAvailable = true;
   for (let idl = 101; idl < 117; idl++) {
     Locker.findOne({ isAvailable: isAvailable }).then
-  }
-});
+  }*/
 
-
-router.post('/register', (req, res) => {
-  const name = req.body.nombre;
-  const email = req.body.mail;
-  const plate = req.body.mat;
-  const major = req.body.carrera;
-  const password = req.body.password;
-  const password2 = req.body.password2;
-  let errors = [];
-
-  if (!name || !email || !plate || !major || !password || !password2) {
-    errors.push({ msg: 'Please enter all fields' });
-  }
-
-  if (password != password2) {
-    errors.push({ msg: 'Passwords do not match' });
-  }
-
-  if (password.length < 6) {
-    errors.push({ msg: 'Password must be at least 6 characters' });
-  }
-
-  if (plate.length < 9) {
-    errors.push({ msg: 'Plate must include A or LC' });
-  }
-
-  if (plate.length == 10){
-    isAdmin = true;
-  }
-
-  if (errors.length > 0) {
-    res.render('register', {
-      errors,
-      name,
-      email,
-      plate,
-      password,
-      password2
-    });
-  } else {
-    User.findOne({ email: email }).then(user => {
-      if (user) {
-        errors.push({ msg: 'Email already exists' });
-        res.render('register', {
-          errors,
-          name,
-          email,
-          plate,
-          password,
-          password2
-        });
-      } else {
-        const newUser = new User({
-          name,
-          email,
-          plate,
-          major,
-          password,
-          isAdmin
-        });
-
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser
-              .save()
-              .then(user => {
-                req.flash(
-                  'success_msg',
-                  'Ya estas registrado puedes acceder'
-                );
-                res.redirect('/users/login');
-              })
-              .catch(err => console.log(err));
-          });
-        });
-      }
-    });
-  }
-});
+//const available = Locker.findOne({isAvailable:true});
+//res.render('lockers.ejs');
+// Revisar si hubo respuesta
+/*
+if (!available){
+  // Regresar mensaje
+  // ******
+}
+else {
+  const idLocker = available.idl;
+  Lockerdb.update({idl:idLocker}, {
+    who: req.user.name,
+    isAvailable: false
+  })
+  //actualizar este locker
+  //User.update({})
+}
 */
-
+//});
 
 // donations view
 router.get('/donations', ensureAuthenticated, (req, res) =>
   res.render('donations.ejs', {
-    name: req.user.name
+    name: req.user.name,
+    isAdmin: req.user.isAdmin
   })
 );
 
 // reservations view
 router.get('/reservations', ensureAuthenticated, (req, res) =>
   res.render('reservations.ejs', {
-    name: req.user.name
+    name: req.user.name,
+    isAdmin: req.user.isAdmin
+  })
+);
+
+// admin view
+router.get('/admin', ensureAuthenticated, (req, res) =>
+  res.render('admin.ejs', {
+    name: req.user.name,
+    isAdmin: req.user.isAdmin
   })
 );
 
